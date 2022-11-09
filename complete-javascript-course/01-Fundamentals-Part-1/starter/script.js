abd98eda9e15b0bc6dfa24a2431cb306f8dadf85
crist6494 <moralesrojascr@gmail.com>
2022-11-09 12:27:59 +0100

    Udemy-js

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,39 @@
+////////////////////////////////////
+// Linking a JavaScript File
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+////////////////////////////////////
+// Values and Variables
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Cristian";
+
+console.log(firstName);
+
+/*Exercise */
+
+let country = "Spain";
+let continent = "Europe";
+let population = 47000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+////////////////////////////////////
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
