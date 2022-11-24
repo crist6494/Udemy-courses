e294b1accd18861381372492cf0586cc290e47dc
crist6494 <moralesrojascr@gmail.com>
2022-11-24 12:17:34 +0100

    Udemy-js

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -92,7 +92,7 @@ console.log(firstName + " " + lastName);
 //Assignment operators
 let x = 10 + 5;
 x += 10; //x = x + 10
-x *= 4   //x = x *
+x *= 4   //x = x * 4
 x++;     //x = x + 1
 x--;
 console.log(x);
@@ -102,7 +102,7 @@ console.log(ageJonas > ageCristian); // >, <, >=, <=
 console.log(ageCristian >= 19);
 
 const fullAge = ageCristian >= 19;
-console.log(now - 1991 > now - 2018);*/
+console.log(now - 1991 > now - 2018);
 
 /////////////////////////////////////////////
 //-----Operator Precedence------//
@@ -122,3 +122,220 @@ const averageAge = (ageJonas + ageCristian) / 2;
 console.log(ageJonas, ageCristian, averageAge)
 
 ///////////////////////////////////////////////////
+/-----Coding Challenge #1-----/
+
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall
+
+//Data 1
+//let massMark = 78;
+//let heightMark = 1.69;
+//let massJohn = 92;
+//let heightJohn = 1.95; 
+
+//Data 2
+let massMark = 95;
+let heightMark = 1.88;
+let massJohn = 85;
+let heightJohn = 1.76;
+
+const MBIMark = massMark / heightMark ** 2;
+const MBIJohn = massJohn / (heightJohn * heightJohn);
+console.log(MBIMark,MBIJohn);
+
+const markHigherBMI = MBIMark > MBIJohn;
+console.log(markHigherBMI)
+
+/////////////////////////////////////////////////////////////
+//-----Strings and Template Literals------//
+
+const firstName = "Cristian";
+const job = "programmer";
+const birthYear = 2003;
+const year = 2022;
+
+const cristian = "I'm " + firstName + ", a " + (year - birthYear) + 
+" years old " + job + "!";
+console.log(cristian)
+
+const cristianTemplateLiteral = `I'm ${firstName}, a ${year - 
+birthYear} years old ${job}!`
+console.log(cristianTemplateLiteral)
+
+console.log(`Just a regular string ...`)
+
+console.log("String with \n\
+mulitple\n\
+lines");
+
+console.log(`String with 
+mulitple
+lines`);
+
+////////////////////////////////////////////////////////////////////
+//-----Taking decisions: if/else Statements------//
+
+const age = 15;
+
+if(age >= 18){
+	console.log("Sarah can start driving license 🚗")
+}else{
+	const yearsLeft = 18 - age;
+	console.log(`Sarah is too young, Wait another ${yearsLeft} years :)`)
+}
+
+const birthYear = 2003;
+let century;
+
+if(birthYear <= 2000){
+	century = 20;
+}else{
+	century = 21;
+}
+console.log(century);
+
+//////////////////////////////////////////////////////////////////////
+//-----Coding Challenge #2-----//
+
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement 
+
+//Data 1
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95; 
+
+//Data 2
+//let massMark = 95;
+//let heightMark = 1.88;
+//let massJohn = 85;
+//let heightJohn = 1.76; 
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark,BMIJohn);
+
+if(BMIMark > BMIJohn){;
+	console.log(`Mark's BMI ${BMIMark} is higher than John's${BMIJohn}!`)
+}else{
+	console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+}
+
+///////////////////////////////////////////////////////////////////////////
+//-------Type conversion and coercion-------//
+
+//Type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//Type coersion
+console.log("I 'am " + 23 + " years old");
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
+
+////////////////////////////////////////////////////////////////////////////
+//-------Truthy and Falsy Values-------//
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+  console.log('YAY! Height is defined');
+} else {
+  console.log('Height is UNDEFINED');
+}
+
+////////////////////////////////////////////////////////////////////////////
+//-----Equality Operators: == vs. ===-----//
+
+const age = "18";
+if(age === 18) console.log("You just became an adult :D (strict equal");
+
+if(age == 18) console.log("You just became an adult :D (loose equal)")
+
+
+const favourite = Number(prompt("What's your favouite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if(favourite === 23){  
+	console.log("Cool! 23 is amazing number!");
+}else if(favourite === 7){
+	console.log("7 is also a cool number");
+}else if(favourite === 9){
+	console.log("9 is also a cool number");
+}else{
+	console.log("Number is not 23 or 7 or 9");
+}
+
+if(favourite !== 23) console.log("Why not 23?")*/
+
+////////////////////////////////////////////////////////////////////////////
+//-------Logical Operators-------//
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+//if(shouldDrive){
+	//console.log("Sarah is able to drive!")
+//}else{
+	//console.log("Someone else should drive...")
+//}
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if(hasDriversLicense && hasGoodVision && !isTired){
+	console.log("Sarah is able to drive!")
+}else{
+	console.log("Someone else should drive...")
+}
