b6fff0790e602d534886b068acf1be3a8065ec1a
crist6494 <moralesrojascr@gmail.com>
2022-11-30 12:06:00 +0100

    Udemy-js

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -311,7 +311,7 @@ if(favourite === 23){
 	console.log("Number is not 23 or 7 or 9");
 }
 
-if(favourite !== 23) console.log("Why not 23?")*/
+if(favourite !== 23) console.log("Why not 23?")
 
 ////////////////////////////////////////////////////////////////////////////
 //-------Logical Operators-------//
@@ -338,4 +338,128 @@ if(hasDriversLicense && hasGoodVision && !isTired){
 	console.log("Sarah is able to drive!")
 }else{
 	console.log("Someone else should drive...")
+}*/
+
+////////////////////////////////////////////////////////////////////////////
+//-------Coding Challenge #3-------//
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+
+/* const mediaDolphins = (96 + 108 + 89) / 3; 
+const mediaKoalas = (88 + 91 + 110) / 3; 
+console.log(mediaDolphins, mediaKoalas)
+
+if(mediaDolphins > mediaKoalas){
+	console.log("Dolphins win the trophy")
+}else if(mediaDolphins < mediaKoalas){
+	console.log("Koalas win the trophy")
+}else if(mediaDolphins === mediaKoalas){
+	console.log("There is a draw between both teams")
+} 
+
+//BONUS 1
+
+const mediaDolphins = (97 + 112 + 101) / 3; 
+const mediaKoalas = (109 + 95 + 123) / 3; 
+console.log(mediaDolphins, mediaKoalas)
+
+if(mediaDolphins > mediaKoalas && mediaDolphins >= 100){
+	console.log("Dolphins win the trophy");
+}else if(mediaDolphins < mediaKoalas && mediaKoalas >= 100){
+	console.log("Koalas win the trophy");
+}else if(mediaDolphins === mediaKoalas && mediaDolphins>= 100 && mediaKoalas >= 100){
+	console.log("There is a draw between both teams")
+}else{
+	console.log("No one wins the trophy");
 }
+
+
+////////////////////////////////////////////////////////////////////////////
+//-------The switch Statement-------//
+
+const day = "wednesday";
+
+switch(day){
+	case "monday": //day === "monday"
+		console.log("Plan my course structure");
+		console.log("Go to coding meetup");
+		break;
+	case "tuesday":
+		console.log("Prepare theory videos");
+		break;
+	case"wednesday":
+	case"thursday":
+		console.log("Write code examples");
+		break;
+	case"friday":
+		console.log("Write code examples");
+		break;
+	case"friday":
+	case"saturday":
+		console.log("Enjoy the weekend :D");
+		break;
+	default:
+		console.log("Not a valid day");
+}
+
+if(day === "monday"){
+	console.log("Plan my course structure");
+	console.log("Go to coding meetup");
+}else if(day === "tuesday"){
+	console.log("Prepare theory videos");
+}else if(day === "wednesday" || day == "thursday"){
+	console.log("Write code examples");
+}else if(day === "friday"){
+	console.log("Enjoy the weekend :D");
+}else if(day === "friday" || day == "saturday"){
+	console.log("Enjoy the weekend :D");
+}else{
+	console.log("Not a valid day");
+}
+
+////////////////////////////////////////////////////////////////////////////
+//-------Statement and Expressions-------//
+
+3 + 4
+1991
+true && false && !false
+
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`);*/
+
+////////////////////////////////////////////////////////////////////////////
+//-------The Conditional (Ternary) Operator------//
+
+const age = 17;
+//age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink); 
+
+let drink2;
+if (age >= 18) {
+	drink2 = 'wine 🍷';
+  } else {
+	drink2 = 'water 💧';
+}
+console.log(drink2); 
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
